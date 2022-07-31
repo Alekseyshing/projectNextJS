80dae3d486094c04e0909be6b79f07333efe444a
Aleksey <alekseytim@mail.ru>
2022-07-31 09:44:36 +0300

    add: TopPageComponent

---
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -0,0 +1,13 @@
+import { TopPageComponentProps } from "./TopPageComponent.props";
+import styles from './P.module.css';
+import cn from 'classnames';
+
+
+export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
+
+  return (
+    <div>
+      {products && products.length}
+    </div>
+  )
+}
--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -6,18 +6,19 @@ import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface'
 import { ParsedUrlQuery } from 'querystring';
 import { ProductModel } from '../../interfaces/product.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
+import { TopPageComponent } from '../../page-components';
 
 
-function Course({ menu, page, products }: CourseProps): JSX.Element {
+function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
 
-  return (
-    <div>
-      {products && products.length}
-    </div>
-  );
+  return <TopPageComponent
+    firstCategory={firstCategory}
+    page={page}
+    products={products}
+  />
 }
 
-export default withLayout(Course);
+export default withLayout(TopPage);
 
 export const getStaticPaths: GetStaticPaths = async () => {
   let paths: string[] = [];
@@ -33,7 +34,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps<CourseProps> = async ({ params }: GetStaticPropsContext<ParsedUrlQuery>) => {
+export const getStaticProps: GetStaticProps<TopPageProps> = async ({ params }: GetStaticPropsContext<ParsedUrlQuery>) => {
   if (!params || !params.alias || typeof (params.alias) !== 'string') {
     return {
       notFound: true
@@ -52,7 +53,7 @@ export const getStaticProps: GetStaticProps<CourseProps> = async ({ params }: Ge
     const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
       firstCategory: firstCategoryItem.id
     })
-    if(menu.length === 0){
+    if (menu.length === 0) {
       return {
         notFound: true
       }
@@ -78,7 +79,7 @@ export const getStaticProps: GetStaticProps<CourseProps> = async ({ params }: Ge
   }
 }
 
-interface CourseProps extends Record<string, unknown> {
+interface TopPageProps extends Record<string, unknown> {
   menu: MenuItem[];
   firstCategory: TopLevelCategory;
   page: TopPageModel;
