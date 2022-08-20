84aae4f9a64f699effacde8ed60a989e6ac6182a
Aleksey <alekseytim@mail.ru>
2022-08-20 13:00:03 +0300

    feat: meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return <>
     <Head>
       <title>My top - наш лучший топ</title>
@@ -10,6 +11,8 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
+      <meta property="og:locale" content="ru_Ru"/>
     </Head>
     <Component {...pageProps} />
   </>;
